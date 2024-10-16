0c7472f1b338d2400435cb94ef2c84f864e5308b
AyoubElouazzani <elouazzaniayoub05@gmail.com>
2024-10-16 23:23:28 +0200

    elemination the host on the creation room form

--- base/forms.py
+++ base/forms.py
@@ -5,3 +5,4 @@ class RoomForm(forms.ModelForm):
     class Meta:
         model = Room
         fields = '__all__'  # This includes all fields from the Room modelf
+        exclude = ['host', 'participants'] 
