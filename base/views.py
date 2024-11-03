a810c52f6c395f4a7263e3f4fffc1e7dcada3a7d
AyoubElouazzani <elouazzaniayoub05@gmail.com>
2024-11-03 23:04:37 +0100

    make some solution in the room page version  2

--- base/views.py
+++ base/views.py
@@ -54,21 +54,25 @@ def room(request,pk):
 @login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
+    topics = Topic.objects.all()
     if request.method == 'POST':
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            room = form.save(commit = False)
-            room.host = request.user
-            room.save()
-            return redirect('home')
-
-    context={'form':form}
+        topic_name = request.POST.get('topic')
+        topic,created = Topic.objects.get_or_create(name=topic_name)
+        Room.objects.create(
+            host=request.user,
+            topic = topic,
+            name=request.POST.get('name'),
+            description=request.POST.get('description'),
+        )
+        return redirect('home')
+    context={'form':form,'topics':topics}
     return render(request,'base/room_form.html',context)
 
 @login_required(login_url='login')
 def updateRoom(request,pk):
     room = Room.objects.get(id=pk)
     form=RoomForm(instance=room)
+    topics = Topic.objects.all()
 
     if request.user != room.user:
         return HttpResponse('You do not own this room')
@@ -78,7 +82,7 @@ def updateRoom(request,pk):
         if form.is_valid():
             form.save()
             return redirect('home')
-    context={'form':form}
+    context={'form':form,'topics':topics}
     return render(request,'base/room_form.html',context)
 
 
